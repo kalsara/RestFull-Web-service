2ec88651862002ae476470c9ae945bc4eda1e8fa
kalsara <kalsaramagamage@gmail.com>
2017-03-02 19:14:51 +0530

    edit service class

--- src/main/java/com/mycompany/testws/services/Service.java
+++ src/main/java/com/mycompany/testws/services/Service.java
@@ -1,6 +1,20 @@
 
 public class Service {
     
+  private static Map<Integer,person> persons=new HashMap<Integer,person>();
     
+    static {
+       for(int i=0;i<10;i++){
+       person person=new person();
+       int id=i+1;
+       person.setId(id);
+       person.setFullName("hi"+id);
+       person.setAge(new Random().nextInt(40)+1);
+       
+       persons.put(id,person);
+       
+       }
+    
+    }  
     
 }
