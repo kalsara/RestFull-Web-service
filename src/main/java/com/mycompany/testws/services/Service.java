240bd3b465ca599a010a8647d207f27b406cdd26
kalsara <kalsaramagamage@gmail.com>
2017-03-02 19:19:28 +0530

    connect service and person class

--- src/main/java/com/mycompany/testws/services/Service.java
+++ src/main/java/com/mycompany/testws/services/Service.java
@@ -1,7 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testws.services;
 
+import com.mycompany.testws.model.person;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author kalsara
+ */
+@Path("Service")
 public class Service {
     
-  private static Map<Integer,person> persons=new HashMap<Integer,person>();
+    private static Map<Integer,person> persons=new HashMap<Integer,person>();
     
     static {
        for(int i=0;i<10;i++){
@@ -15,6 +38,32 @@ public class Service {
        
        }
     
-    }  
+    }
+    
+    @GET
+    @Path("getpersonByIdXML/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public person getpersonByIdXML(@PathParam("id")int id){
+       
+        return persons.get(id);
+    }
+    
+    @GET
+    @Path("getpersonByIdJASON/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public person getpersonByIdJASON(@PathParam("id")int id){
+       
+        return persons.get(id);
+    }
+    
+    
+    @GET
+    @Path("getAllpersonsInXML/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<person> getAllpersonsInXML(){
+       
+        return new ArrayList<person>(persons.values());
+    }
+    
     
 }
