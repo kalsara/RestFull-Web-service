42ad4f1a6aab51a5e0abae4c2006b8ba8bf0c76f
kalsara <kalsaramagamage@gmail.com>
2017-03-02 18:56:51 +0530

    edit person class

--- src/main/java/com/mycompany/testws/model/person.java
+++ src/main/java/com/mycompany/testws/model/person.java
@@ -7,6 +7,38 @@
 
 public class person {
     
+    @XmlRootElement(name="person")
+@XmlType(propOrder={"id","fullName","age"})
+public class person {
+    
+    private int id;
+    private String fullName;
+    private int age;
+@XmlElement
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+@XmlElement
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+@XmlElement
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
     
     
 }
